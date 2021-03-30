2ac35aee5f2883e44fc1ba28e0562c3fbc320f2e
Vamsinandan BS <61873858+vamsinandanBs@users.noreply.github.com>
2021-03-30 06:36:37 +0530

    Add files via upload

---
+++ Snow fall/style.js
@@ -0,0 +1,20 @@
+function createSnow(){
+    const snow = document.createElement('div');
+
+    snow.classList.add('snow');
+
+    snow.style.right=Math.random() *100 + "vw";
+
+    snow.style.animationDuration = Math.random() + 3 + "s";
+
+    snow.innerText = "❄️";
+    
+
+    document.body.appendChild(snow);
+
+    setTimeout(() =>{
+        snow.remove();
+    },5000);
+}
+
+setInterval(createSnow,300);
